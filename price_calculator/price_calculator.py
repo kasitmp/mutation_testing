176da07f17b28e9ca402436fdf5d36274c352b87
Karsten Lettow <kasitmp@googlemail.com>
2025-03-30 17:17:56 +0200

    Add nevada tax

--- price_calculator/price_calculator.py
+++ price_calculator/price_calculator.py
@@ -2,6 +2,11 @@ class PriceCalculator:
     def order_value(self, count: int, price: float) -> float:
         return count * price
 
-    def taxed_order_value(self, count: int, price: float) -> float:
+    def apply_tax(self, order_value, tax: float = 0.0685) -> float:
+        return order_value + order_value * tax
+
+    def total_value(self, count: int, price: float, state: str):
         order_value = self.order_value(count, price)
-        return order_value + order_value * 0.0685
+        if state == "UT":
+            return self.apply_tax(order_value)
+        return self.apply_tax(order_value, 0.08)
--- tests/test_price_calculator.py
+++ tests/test_price_calculator.py
@@ -14,11 +14,28 @@ class TestOrderValue:
         assert self._calculator.order_value(0, 2.0) == 0.0
 
 
-class TestUtahTax:
+class TestApplyUtahTax:
     _calculator: PriceCalculator = PriceCalculator()
 
     def test_taxed_order_value(self):
-        assert self._calculator.taxed_order_value(1, 1.0) == 1.0685
+        assert self._calculator.apply_tax(1.0) == 1.0685
 
-    def test_taxed_order_different_price(self):
-        assert self._calculator.taxed_order_value(1, 2.0) == 2.137
+    def test_taxed_order_value_different(self):
+        assert self._calculator.apply_tax(2.0) == 2.137
+
+
+class TestFlexibleTax:
+    _calculator: PriceCalculator = PriceCalculator()
+
+    def test_taxed_order_value_nevada(self):
+        assert self._calculator.apply_tax(1.0, 0.08) == 1.08
+
+
+class TestTotalPrice:
+    _calculator: PriceCalculator = PriceCalculator()
+
+    def test_nevada(self):
+        assert self._calculator.total_value(1, 1.0, "NV") == 1.08
+
+    def test_utah(self):
+        assert self._calculator.total_value(1, 1.0, "UT") == 1.0685
