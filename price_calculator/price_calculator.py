07443d50a4a67653dfcf58e99f244240790517e9
Karsten Lettow <kasitmp@googlemail.com>
2025-03-30 16:56:48 +0200

    added utah tax

--- price_calculator/price_calculator.py
+++ price_calculator/price_calculator.py
@@ -1,3 +1,7 @@
 class PriceCalculator:
     def order_value(self, count: int, price: float) -> float:
         return count * price
+
+    def taxed_order_value(self, count: int, price: float) -> float:
+        order_value = self.order_value(count, price)
+        return order_value + order_value * 0.0685
--- tests/test_price_calculator.py
+++ tests/test_price_calculator.py
@@ -12,3 +12,13 @@ class TestOrderValue:
 
     def test_zero_item_order(self):
         assert self._calculator.order_value(0, 2.0) == 0.0
+
+
+class TestUtahTax:
+    _calculator: PriceCalculator = PriceCalculator()
+
+    def test_taxed_order_value(self):
+        assert self._calculator.taxed_order_value(1, 1.0) == 1.0685
+
+    def test_taxed_order_different_price(self):
+        assert self._calculator.taxed_order_value(1, 2.0) == 2.137
